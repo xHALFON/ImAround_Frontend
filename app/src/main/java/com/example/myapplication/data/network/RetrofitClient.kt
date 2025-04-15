b9c3fb367a9d0123f9d1fd514d0502638eae2e0f
Omer Sharivker <sharivker90@gmail.com>
2025-04-15 18:20:49 +0300

    new ui for SearchScreen

--- app/src/main/java/com/example/myapplication/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/myapplication/data/network/RetrofitClient.kt
@@ -7,7 +7,7 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.68.66:3000/"
+    private const val BASE_URL = "http://10.0.2.2:3000/"
 
     // יצירת Retrofit instance אחד
     private val retrofit: Retrofit by lazy {
